d5025d196bb34c77abd6a060a937c0bd9124f4e1
jinsu291 <wlstn291@naver.com>
2022-10-01 21:35:41 +0900

    세팅

---
+++ src/main/java/com/ll/exam/app_2022_10_01/App20221001Application.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app_2022_10_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App20221001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App20221001Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/app_2022_10_01/App20221001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app_2022_10_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App20221001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
