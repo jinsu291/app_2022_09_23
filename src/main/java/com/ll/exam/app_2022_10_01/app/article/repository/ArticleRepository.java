2e670896963a213ef906de7c8a4ebdf71a50fc57
jinsu291 <wlstn291@naver.com>
2022-10-02 07:42:27 +0900

    게시물 작성 후 생성된 ID를 반환

--- src/main/java/com/ll/exam/app_2022_10_01/app/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/app_2022_10_01/app/article/repository/ArticleRepository.java
@@ -29,4 +29,9 @@ public interface ArticleRepository {
             </script>
             """)
     void write(@Param("subject") String subject, @Param("content") String content);
+
+    @Select("""
+            SELECT LAST_INSERT_ID()
+            """)
+    public long getLastInsertId();
 }
--- src/test/java/com/ll/exam/app_2022_10_01/AppTest.java
+++ src/test/java/com/ll/exam/app_2022_10_01/AppTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
@@ -26,6 +27,8 @@ class AppTest {
 	@Test
 	@DisplayName("게시물 작성")
 	void t2() {
-		articleService.write("제목3", "내용3");
+		long id = articleService.write("제목3", "내용3");
+
+		assertThat(id).isGreaterThan(0);
 	}
 }
