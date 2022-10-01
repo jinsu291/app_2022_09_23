c46b8d7880369dcf07977274e9dcda53357e4b99
jinsu291 <wlstn291@naver.com>
2022-10-02 07:26:02 +0900

    게시물 등록

--- src/main/java/com/ll/exam/app_2022_10_01/app/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/exam/app_2022_10_01/app/article/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package com.ll.exam.app_2022_10_01.app.article.repository;
 
 import com.ll.exam.app_2022_10_01.app.article.dto.Article;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -16,4 +18,15 @@ public interface ArticleRepository {
             </script>
             """)
     List<Article> getArticles();
+
+    @Insert("""
+            <script>
+            INSERT INTO article
+            SET createDate = NOW(),
+            modifyDate = NOW(),
+            subject = #{subject},
+            content = #{content}
+            </script>
+            """)
+    void write(@Param("subject") String subject, @Param("content") String content);
 }
